1f173f1a7233fd2abae038d88b4e1308a1ca2441
naranaila8 <naranaila8@gmail.com>
2026-09-01 13:17:10 +0700

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 # --- PENGATURAN HALAMAN ---
 st.set_page_config(page_title="Keuangan Keluarga", layout="wide")
-st.title("💰 Rumi-Isa Keuangan")
+st.title("💰 Rumi-Isa Future" )
 
 # --- KONEKSI KE GOOGLE SHEETS ---
 conn = st.connection("gsheets", type=GSheetsConnection)
